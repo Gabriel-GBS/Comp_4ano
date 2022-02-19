cb855f3e9405c2b83014177b0c4f54959851dca8
Gabriel Gomes <bermudisantos.gg@gmail.com>
2022-02-19 12:28:44 -0300

    retomar da sessão 1.2, falando de tipos

---
+++ index.js
@@ -0,0 +1,35 @@
+// JS não preve algns erros no compilador, além da redeclaração, podemos chamar uma váriavel não declarada antes, como no ex abaixo (com var)
+var idade = 18
+console.log('Oi, ' + nome)
+if (idade >= 18){
+ var nome = 'Cria'
+ console.log('Parabéms, ' + nome)
+ console.log('Você é o klica')
+}
+console.log('Até mais ' + nome)
+
+// // possível redeclarar variáveis, mas não é o ideal (com var)
+// var linguagem = 'JavaScript'
+// console.log ('Aprendendo ' + linguagem)
+// //variável ´pde ser redeclarada
+// var linguagem = 'Java'
+// console.log('Aprendendo ' + linguagem)
+
+// //const - constante - não muda o tipo
+// const nome = 'GominDaZs'
+// const endereco = `Rua X, 27`
+// const sexo = 'M'
+// const idade = 21
+
+// //let - tipo variável
+// let a = 2
+// let nomeCompleto = 'Joao Silva'
+// a = "abc"
+
+// //var - basicamente igual ao let - escopos diferentes - var pouco intuitivo
+// var c = 2+3
+// var d = 'abcd'
+
+
+
+
