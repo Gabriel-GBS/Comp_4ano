4316082795bef8826e39b58284d39dbc31806221
Gabriel-GBS <bermudisantos.gg@gmail.com>
2022-03-26 11:09:45 -0300

    criacao index.js

---
+++ Lembretes_css/lembretes/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+//GET
+//exemplo.com.br/lembretes
+app.get('/lembretes', (req, res) => {
+
+})
+
+//POST
+//exemplo.com.br/lembretes
+app.post('/lembretes', (req, res) => {
+
+})
+
