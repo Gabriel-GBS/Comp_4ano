a20d2ed7c2ffd4830bd3072d40b4b6892cb5608a
Gabriel-GBS <bermudisantos.gg@gmail.com>
2022-03-26 11:58:04 -0300

    atualizacao index

--- Lembretes_css/lembretes/index.js
+++ Lembretes_css/lembretes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+
 //GET
 //exemplo.com.br/lembretes
 app.get('/lembretes', (req, res) => {
@@ -12,3 +13,6 @@ app.post('/lembretes', (req, res) => {
 
 })
 
+app.listen(4000, () => {
+    console.log('Lembretes. Porta 4000.')
+})
